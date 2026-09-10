4e8eeae1d85b80b8d72d7ea4852741e612e31416
molokokos89-rgb <molokokos89@gmail.com>
2026-09-10 11:37:01 +0500

    Update collector.py

--- collector.py
+++ collector.py
@@ -17,12 +17,14 @@ from collections import defaultdict
 
 # --- источники (рабочие на 2026-09) ---
 SOURCES = [
-    # EbraSha — основные hy2 (рабочие пути)
-    "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/main/BLACK_SS%2BAll_RUS.txt"
+    "https://raw.githubusercontent.com/yebekhe/TelegramV2rayCollector/main/sub/normal/mix",
+    "https://raw.githubusercontent.com/freefq/free/master/v2ray",
+    "https://raw.githubusercontent.com/barry-far/V2ray-Configs/main/All_Configs_Sub.txt",
+    "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/main/BLACK_VLESS_RUS.txt",
+    "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/main/Vless-Reality-White-Lists-Rus-Mobile.txt"
 ]
 
-# vless специально НЕ берём
-PROTOCOLS = ["hy2://", "hysteria2://", "trojan://", "ss://", "vmess://"]
+PROTOCOLS = ["ss://", "vmess://", "trojan://", "hy2://", "hysteria2://", "vless://"]
 
 PROTOCOL_PRIORITY = {
     "hy2://": 0,
